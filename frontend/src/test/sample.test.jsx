b31fbe0c5b6a9534383c0c2c4f425abe0d37ef6f
izzat mashrom <mohdizzatmashrom@gmail.com>
2026-08-08 12:22:01 +0800

    fix error

--- frontend/src/test/sample.test.jsx
+++ frontend/src/test/sample.test.jsx
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import LoadingMessage from '../components/LoadingMessage';
 import ErrorMessage from '../components/ErrorMessage';
